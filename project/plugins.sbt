145cf060127bb270fad24b60019926bc10f9d8d9
hongwei1 <hhh@tesobe.com>
2016-12-17 12:59:20 +0100

    5 Files to set up Lift
    The basic liftweb is working
    
    #http://chimera.labs.oreilly.com/books/1234000000030/ch01.html#_solution_2

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,8 +1,2 @@
-//Enable the sbt web plugin
+//Enable the sbt web plugin : This plugin will allow you to start and stop your application.
 addSbtPlugin("com.earldouglas" % "xsbt-web-plugin" % "0.7.0")
-
-//Enable the sbt idea plugin
-addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.6.0")
-
-//Enable the sbt eclipse plugin
-addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "4.0.0")
--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -1,20 +1,7 @@
-
-
-
-
 package bootstrap.liftweb
 
-import code.model._
-import net.liftmodules.JQueryModule
-import net.liftweb._
-import net.liftweb.common._
-import net.liftweb.http._
-import net.liftweb.http.js.jquery.JQueryArtifacts
-import net.liftweb.mapper._
-import net.liftweb.sitemap.Loc._
-import net.liftweb.sitemap._
-import net.liftweb.util._
-
+import net.liftweb.http.{Html5Properties, LiftRules, Req}
+import net.liftweb.sitemap.{Menu, SiteMap}
 
 /**
   * A class that's instantiated early and run.  It allows the application
@@ -22,75 +9,16 @@ import net.liftweb.util._
   */
 class Boot {
   def boot {
-    if (!DB.jndiJdbcConnAvailable_?) {// check if there is using the DBVendor object
-      sys.props.put("h2.implicitRelativePath", "true")
-      val vendor =  new StandardDBVendor(
-          //          Props.get("db.driver") openOr "org.h2.Driver",
-          //          Props.get("db.url") openOr "jdbc:h2:lift_proto.db;AUTO_SERVER=TRUE",
-          //          Props.get("db.user"), Props.get("db.password"))
-
-          Props.get("db.driver") openOr "org.postgresql.Driver",
-          Props.get("db.url") openOr "jdbc:postgresql://localhost:5432/myLift",
-          Props.get("db.user"), Props.get("db.password"))
-      DB.defineConnectionManager(util.DefaultConnectionIdentifier, vendor)
-
-      LiftRules.unloadHooks.append(vendor.closeAllConnections_! _)
-
-    }
-//    /Users/zhanghongwei/Desktop/lift-lift_26_sbt-3c4742e/scala_211/lift_basic
-    //
-    //
-    // /src/main/scala/bootstrap/liftweb/Boot.scala
-
-
-
-    // Use Lift's Mapper ORM to populate the database
-    // you don't need to use Mapper to use Lift... use
-    // any ORM you want
-    Schemifier.schemify(true, Schemifier.infoF _, User,Author, BookAuthors, Book, Publisher)
-
     // where to search snippet
-    LiftRules.addToPackages("code")
+    LiftRules.addToPackages("org.yourorganization.liftfromscratch")
 
     // Build SiteMap
-    def sitemap = SiteMap(
-      Menu.i("Home") / "index" >> User.AddUserMenusAfter, // the simple way to declare a menu
-
-      // more complex because this menu allows anything in the
-      // /static path to be visible
-      Menu(Loc("Static", Link(List("static"), true, "/static/index"),
-        "Static Content")))
-
-    def sitemapMutators = User.sitemapMutator
-
-    // set the sitemap.  Note if you don't want access control for
-    // each page, just comment this line out.
-    LiftRules.setSiteMapFunc(() => sitemapMutators(sitemap))
-
-    //Init the jQuery module, see http://liftweb.net/jquery for more information.
-    LiftRules.jsArtifacts = JQueryArtifacts
-    JQueryModule.InitParam.JQuery = JQueryModule.JQuery191
-    JQueryModule.init()
-
-    //Show the spinny image when an Ajax call starts
-    LiftRules.ajaxStart =
-      Full(() => LiftRules.jsArtifacts.show("ajax-loader").cmd)
-
-    // Make the spinny image go away when it ends
-    LiftRules.ajaxEnd =
-      Full(() => LiftRules.jsArtifacts.hide("ajax-loader").cmd)
-
-    // Force the request to be UTF-8
-    LiftRules.early.append(_.setCharacterEncoding("UTF-8"))
-
-    // What is the function to test if a user is logged in?
-    LiftRules.loggedInTest = Full(() => User.loggedIn_?)
+    def sitemap(): SiteMap = SiteMap(
+      Menu.i("Home") / "index"
+    )
 
     // Use HTML5 for rendering
     LiftRules.htmlProperties.default.set((r: Req) =>
       new Html5Properties(r.userAgent))
-
-    // Make a transaction span the whole HTTP request
-    S.addAround(DB.buildLoanWrapper)
   }
 }
